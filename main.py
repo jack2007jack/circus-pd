fae6ffe263cef39e764048d19b2dda288d870c23
Ilya <karpov.ilja@gmail.com>
2024-02-08 15:59:43 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+# Specifikācija
+# - 1pt Spelētāji sāk no lauciņa nr. 1, vispār 100 lauciņu. Ir divi spēlētāji. Vinē tas kurš pirmais sasniedz pēdējo lauciņu
+# - 1pt Maksimāli - 30 raundi, ja beidzas raundi - neizšķirts
+# - 1pt Viens pēc otra met kauliņu (ar nejauša ciparu ģenerātora palidzību) un iet uz priekšu
+# - 1pt Ja trāpa uz lauciņu ar kāpnem:
+# -- zilas kāpnes ved uz leju, 16 -> 5, 65 -> 44 , 78 -> 67, 72 -> 61
+# -- sarkanas kāpnes ved uz augšu, 13 -> 22, 37 -> 46, 31 -> 50, 85 -> 94 
+# - 1pt Katrā raundā tik drukāta informācija kur atrodas spēlētājs, dators un vai ir uzkāpts uz kāpnem
+
+# Koda vertēšanas kritēriji
+# - 1pt Kodā izmanto mainīgus, ciklus (for vai while), zarošanu (if)
+# - 1pt Kods strādā bez kļūdam
+# - 1pt Mainīgo un funkciju nosaukumi atspoguļo izmantošanas būtību, bez saisinājumiem, rakstīti snake_case stilā
+# - 1pt Kodam ir jēdzīgi komentāri, pirms "if, for" koda konstrukcijam
+# - 1pt Izmaiņas saglabātas versiju vadības sistēmā Git
+
+# Dokumentācija
+# Mainīgie - https://www.w3schools.com/python/python_variables.asp
+# Operācijas ar mainīgiem - https://www.w3schools.com/python/python_operators.asp
+# Mainīgo drukāšana - https://www.w3schools.com/python/python_variables_output.asp
+# Nosacījumi, zarošana, if ... else - https://www.w3schools.com/python/python_conditions.asp
+# For cikls - https://www.w3schools.com/python/python_for_loops.asp
+# Nejauša skaitļa generēšana - https://www.w3schools.com/python/ref_random_randint.asp
+# Github Fork (repozitorija kopija) - https://docs.github.com/en/pull-requests/collaborating-with-pull-requests/working-with-forks/fork-a-repo
+# Klonēt repozitoriju - hhttps://code.visualstudio.com/docs/sourcecontrol/intro-to-git
